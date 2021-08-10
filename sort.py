79568505ca428041458ad5decb1f9ff1ba84f823
Aash <aash.discover@gmail.com>
2021-08-10 16:50:08 +0530

    adding bubble sort algorithm

--- sort.py
+++ sort.py
@@ -12,14 +12,33 @@ def selection_sort(nums):
     return nums
 
 
+def bubble_sort(nums):
+    iterations = len(nums)
+    successful_pass = False
+    while iterations > 0 and not successful_pass:
+        successful_pass = True
+        for i in range(0, len(nums) - 1):
+            if nums[i] > nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                successful_pass = False
+        iterations -= 1
+    return nums
+
+
 data = [
     ([4, 3, 2, 1], [1, 2, 3, 4]),
     ([1, 3, 2, 4], [1, 2, 3, 4]),
     ([1, 4, 2, 3], [1, 2, 3, 4]),
-    ([1, 3, 3, 1], [1, 1, 3, 3])
+    ([1, 3, 3, 1], [1, 1, 3, 3]),
+    ([1, 2, 3, 4], [1, 2, 3, 4]),
+    ([1, 3, 2, 6, 1], [1, 1, 2, 3, 6])
 ]
 
-@pytest.mark.parametrize("inp, out", data)
+@pytest.mark.parametrize("inp, out", data.copy())
 def test_selection_sort(inp, out):
     assert selection_sort(inp) == out
+
+@pytest.mark.parametrize("inp, out", data.copy())
+def test_bubble_sort(inp, out):
+    assert bubble_sort(inp) == out
 
