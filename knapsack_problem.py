9895158c8c6f3acc73853c07e388929970f31719
Aash <aash.discover@gmail.com>
2021-08-20 17:41:58 +0530

    print iter knapsack

--- knapsack_problem.py
+++ knapsack_problem.py
@@ -20,10 +20,42 @@ def _knapsack(total, weights, values, last):
 def knapsack(total, weights, values):
     return _knapsack(total, weights, values, len(weights) - 1)
 
+def iter_knapsack(total, weights, values):
+    result = []
+    for i in range(0, len(weights)):
+        result.append([0]*(total+1))
+
+    for i in range(0, len(weights)):
+        for j in range(0, total + 1):
+            if j == 0:
+                result[i][j] = 0
+            else:
+                i_weight = weights[i]
+                i_value = values[i]
+                a, b = 0, 0
+                if j >= i_weight:
+                    a = i_value
+                    diff = j - i_weight
+                    if diff >= 0 and i > 0:
+                        a += result[i-1][diff]
+                if i> 0:
+                    b = result[i-1][j]
+                print(i, i)
+                result[i][j] = max(a, b)
+
+    return result[i][j]
+
+
+
+
 
 data = [
     (7, [1, 3, 4, 5], [1, 4, 5, 7], 9),
 ]
+# @pytest.mark.parametrize("total, weights, values, out", data.copy())
+# def test_knapsack(total, weights, values, out):
+#     assert knapsack(total, weights, values) == out
+
 @pytest.mark.parametrize("total, weights, values, out", data.copy())
-def test_knapsack(total, weights, values, out):
-    assert knapsack(total, weights, values) == out
+def test_iter_knapsack(total, weights, values, out):
+    assert iter_knapsack(total, weights, values) == out
